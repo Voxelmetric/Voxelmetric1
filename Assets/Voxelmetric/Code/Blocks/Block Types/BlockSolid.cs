2e2f5c6f737e4baaea536630d97fe91f53b5a387
AlexSTV <alexcrafter@gmail.com>
2015-06-21 14:21:49 +0100

    Removed Confing.Textures: they are no longer needed

--- Assets/Voxelmetric/Code/Blocks/Block Types/BlockSolid.cs
+++ Assets/Voxelmetric/Code/Blocks/Block Types/BlockSolid.cs
@@ -29,13 +29,7 @@ public class BlockSolid : BlockController
 
     public virtual void BuildFace(Chunk chunk, BlockPos pos, MeshData meshData, Direction direction, Block block)
     {
-        BlockBuilder.BuildRenderer(chunk, pos, meshData, direction);
-        BlockBuilder.BuildTexture(chunk, pos, meshData, direction, Config.Textures.Stone);
-        BlockBuilder.BuildColors(chunk, pos, meshData, direction);
-        if (Config.Toggle.UseCollisionMesh)
-        {
-            BlockBuilder.BuildCollider(chunk, pos, meshData, direction);
-        }
+       
     }
 
     public override string Name() { return "solid"; }
