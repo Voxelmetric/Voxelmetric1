23d0499a054a4acc5ddd7c0980cb931987064e02
AlexSTV <alexcrafter@gmail.com>
2015-06-22 23:49:18 +0100

    CrossMesh: Added a collider, Fixed shadows

--- Assets/Voxelmetric/Code/Blocks/Builders/MeshBuilder.cs
+++ Assets/Voxelmetric/Code/Blocks/Builders/MeshBuilder.cs
@@ -6,6 +6,7 @@ public class MeshBuilder {
     public static void CrossMeshRenderer(Chunk chunk, BlockPos pos, MeshData meshData, Tile tilePos, Block block)
     {
         float halfBlock = 0.5005f;
+        float colliderOffest = 0.05f;
         float blockHeight = halfBlock * 2 * (block.data2 / 255f);
 
         meshData.AddVertex(new Vector3(pos.x - halfBlock, pos.y - halfBlock, pos.z + halfBlock));
@@ -39,5 +40,11 @@ public class MeshBuilder {
         meshData.AddQuadTriangles();
         BlockBuilder.BuildTexture(chunk, pos, meshData, Direction.north, tilePos);
         meshData.AddColors(0.5f, 0.5f, 0.5f, 0.5f, 0.5f);
+
+        meshData.AddVertex(new Vector3(pos.x - halfBlock, pos.y - halfBlock + colliderOffest, pos.z + halfBlock), collisionMesh: true);
+        meshData.AddVertex(new Vector3(pos.x + halfBlock, pos.y - halfBlock + colliderOffest, pos.z + halfBlock), collisionMesh: true);
+        meshData.AddVertex(new Vector3(pos.x + halfBlock, pos.y - halfBlock + colliderOffest, pos.z - halfBlock), collisionMesh: true);
+        meshData.AddVertex(new Vector3(pos.x - halfBlock, pos.y - halfBlock + colliderOffest, pos.z - halfBlock), collisionMesh: true);
+        meshData.AddQuadTriangles(collisionMesh:true);
     }
 }
