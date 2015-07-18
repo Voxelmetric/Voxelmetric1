38aeb437f5ced5d37da045b785ce39f2fab028f3
AlexSTV <alexcrafter@gmail.com>
2015-07-19 00:21:56 +0100

    Option cull sides facing same block for non-solids
    Added an option to let non-solid blocks not render faces facing blocks
    of the same type as themselves. Addresses Issue #34

--- Assets/Voxelmetric/Code/Blocks/Block Definitions/CubeDefinition.cs
+++ Assets/Voxelmetric/Code/Blocks/Block Definitions/CubeDefinition.cs
@@ -7,12 +7,14 @@ public class CubeDefinition : BlockDefinition
     public string[] textures = new string[6];
 
     public bool blockIsSolid = true;
+    public bool solidTowardsSameType = true;
 
     public override BlockController Controller()
     {
         BlockCube controller = new BlockCube();
         controller.blockName = blockName;
         controller.isSolid = blockIsSolid;
+        controller.solidTowardsSameType = solidTowardsSameType;
 
         TextureCollection[] textureCoordinates = new TextureCollection[6];
 
--- Assets/Voxelmetric/Code/Blocks/Block Types/BlockSolid.cs
+++ Assets/Voxelmetric/Code/Blocks/Block Types/BlockSolid.cs
@@ -3,27 +3,29 @@ using System.Collections;
 
 public class BlockSolid : BlockController
 {
+    public bool isSolid = true;
+    public bool solidTowardsSameType = true;
 
     public BlockSolid() : base() { }
 
     public override void AddBlockData(Chunk chunk, BlockPos pos, MeshData meshData, Block block)
     {
-        if (!chunk.GetBlock(pos.Add(0, 1, 0)).controller.IsSolid(Direction.down))
+        if (!chunk.GetBlock(pos.Add(0, 1, 0)).controller.IsSolid(Direction.down) && (isSolid || !solidTowardsSameType || chunk.GetBlock(pos.Add(0, 1, 0)) != block))
             BuildFace(chunk, pos, meshData, Direction.up, block);
 
-        if (!chunk.GetBlock(pos.Add(0, -1, 0)).controller.IsSolid(Direction.up))
+        if (!chunk.GetBlock(pos.Add(0, -1, 0)).controller.IsSolid(Direction.up) && (isSolid || !solidTowardsSameType || chunk.GetBlock(pos.Add(0, -1, 0)) != block))
             BuildFace(chunk, pos, meshData, Direction.down, block);
 
-        if (!chunk.GetBlock(pos.Add(0, 0, 1)).controller.IsSolid(Direction.south))
+        if (!chunk.GetBlock(pos.Add(0, 0, 1)).controller.IsSolid(Direction.south) && (isSolid || !solidTowardsSameType || chunk.GetBlock(pos.Add(0, 0, 1)) != block))
             BuildFace(chunk, pos, meshData, Direction.north, block);
 
-        if (!chunk.GetBlock(pos.Add(0, 0, -1)).controller.IsSolid(Direction.north))
+        if (!chunk.GetBlock(pos.Add(0, 0, -1)).controller.IsSolid(Direction.north) && (isSolid || !solidTowardsSameType || chunk.GetBlock(pos.Add(0, 0, -1)) != block))
             BuildFace(chunk, pos, meshData, Direction.south, block);
 
-        if (!chunk.GetBlock(pos.Add(1, 0, 0)).controller.IsSolid(Direction.west))
+        if (!chunk.GetBlock(pos.Add(1, 0, 0)).controller.IsSolid(Direction.west) && (isSolid || !solidTowardsSameType || chunk.GetBlock(pos.Add(1, 0, 0)) != block))
             BuildFace(chunk, pos, meshData, Direction.east, block);
 
-        if (!chunk.GetBlock(pos.Add(-1, 0, 0)).controller.IsSolid(Direction.east))
+        if (!chunk.GetBlock(pos.Add(-1, 0, 0)).controller.IsSolid(Direction.east) && (isSolid || !solidTowardsSameType || chunk.GetBlock(pos.Add(-1, 0, 0)) != block))
             BuildFace(chunk, pos, meshData, Direction.west, block);
     }
 
