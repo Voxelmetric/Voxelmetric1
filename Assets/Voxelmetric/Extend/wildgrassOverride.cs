2e2e30f51102d5b9658a6dda085fb678a35865da
AlexSTV <alexcrafter@gmail.com>
2015-07-26 18:47:37 +0100

    Removed scheduled update on wild grass
    Scheduled update was included as a demo but wild grass is used a lot so
    adding a scheduled update for each one is overkill.

--- Assets/Voxelmetric/Extend/wildgrassOverride.cs
+++ Assets/Voxelmetric/Extend/wildgrassOverride.cs
@@ -7,8 +7,7 @@ public class wildgrassOverride : BlockOverride
     // On create set the height to 10 and schedule and update in 1 second
     public override Block OnCreate(Chunk chunk, BlockPos pos, Block block)
     {
-        chunk.AddScheduledUpdate(pos, 1);
-        block.data2 = 10;
+        block.data2 = 100;
         return block;
     }
 
@@ -17,12 +16,5 @@ public class wildgrassOverride : BlockOverride
     {
         block.data2 += 100;
         chunk.SetBlock(pos, block);
-    }
-
-    //On scheduled update add 100 to the height
-    public override void ScheduledUpdate(Chunk chunk, BlockPos pos, Block block)
-    {
-        block.data2 += 100;
-        chunk.SetBlock(pos - chunk.pos, block);
     }
 }
