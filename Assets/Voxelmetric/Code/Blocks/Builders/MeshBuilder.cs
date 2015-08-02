90cb0ddc430b57a5420dd1ad48f2bcbf6d586cc1
AlexSTV <alexcrafter@gmail.com>
2015-08-02 11:24:47 +0100

    Updates wildgrass offset
    Made offset smaller, between -0.25 and 0.25 and made it vary more from
    block to block

--- Assets/Voxelmetric/Code/Blocks/Builders/MeshBuilder.cs
+++ Assets/Voxelmetric/Code/Blocks/Builders/MeshBuilder.cs
@@ -9,12 +9,14 @@ public class MeshBuilder {
         float colliderOffest = 0.05f * Config.Env.BlockSize;
         float blockHeight = halfBlock * 2 * (block.data2 / 255f);
 
-        float offsetX = (halfBlock * 2 * ((byte)(block.data3 & 0x0F) / 16f)) - halfBlock;
-        float offsetZ = (halfBlock * 2 * ((byte)((block.data3 & 0xF0) >> 4) / 16f)) - halfBlock;
+        float offsetX = (halfBlock * 2 * ((byte)(block.data3 & 0x0F) / 32f)) - (halfBlock/2);
+        float offsetZ = (halfBlock * 2 * ((byte)((block.data3 & 0xF0) >> 4) / 32f)) - (halfBlock/2);
 
         //Converting the position to a vector adjusts it based on block size and gives us real world coordinates for x, y and z
         Vector3 vPos = pos;
+        Vector3 vPosCollider = pos;
         vPos += new Vector3(offsetX, 0, offsetZ);
+
         float blockLight = ( (block.data1/255f) * Config.Env.BlockLightStrength) + (0.8f*Config.Env.AOStrength);
 
         meshData.AddVertex(new Vector3(vPos.x - halfBlock, vPos.y - halfBlock, vPos.z + halfBlock));
@@ -49,10 +51,10 @@ public class MeshBuilder {
         BlockBuilder.BuildTexture(chunk, vPos, meshData, Direction.north, texture);
         meshData.AddColors(blockLight, blockLight, blockLight, blockLight, blockLight);
 
-        meshData.AddVertex(new Vector3(vPos.x - halfBlock, vPos.y - halfBlock + colliderOffest, vPos.z + halfBlock), collisionMesh: true);
-        meshData.AddVertex(new Vector3(vPos.x + halfBlock, vPos.y - halfBlock + colliderOffest, vPos.z + halfBlock), collisionMesh: true);
-        meshData.AddVertex(new Vector3(vPos.x + halfBlock, vPos.y - halfBlock + colliderOffest, vPos.z - halfBlock), collisionMesh: true);
-        meshData.AddVertex(new Vector3(vPos.x - halfBlock, vPos.y - halfBlock + colliderOffest, vPos.z - halfBlock), collisionMesh: true);
+        meshData.AddVertex(new Vector3(vPosCollider.x - halfBlock, vPosCollider.y - halfBlock + colliderOffest, vPosCollider.z + halfBlock), collisionMesh: true);
+        meshData.AddVertex(new Vector3(vPosCollider.x + halfBlock, vPosCollider.y - halfBlock + colliderOffest, vPosCollider.z + halfBlock), collisionMesh: true);
+        meshData.AddVertex(new Vector3(vPosCollider.x + halfBlock, vPosCollider.y - halfBlock + colliderOffest, vPosCollider.z - halfBlock), collisionMesh: true);
+        meshData.AddVertex(new Vector3(vPosCollider.x - halfBlock, vPosCollider.y - halfBlock + colliderOffest, vPosCollider.z - halfBlock), collisionMesh: true);
         meshData.AddQuadTriangles(collisionMesh:true);
     }
 }
--- Assets/Voxelmetric/Extend/wildgrassOverride.cs
+++ Assets/Voxelmetric/Extend/wildgrassOverride.cs
@@ -7,10 +7,10 @@ public class wildgrassOverride : BlockOverride
     // On create set the height to 10 and schedule and update in 1 second
     public override Block OnCreate(Chunk chunk, BlockPos pos, Block block)
     {
-        block.data2 = (byte)(64 + ((chunk.world.noiseGen.Generate(pos.x * 100, pos.y * 100, pos.z * 100) + 1) * 96));
-        int offset1 = (int)((chunk.world.noiseGen.Generate(pos.x* 100, pos.y* 100, pos.z * 100) + 1) * 16);
+        block.data2 = (byte)(64 + ((chunk.world.noiseGen.Generate(pos.x * 1000, pos.y * 1000, pos.z * 1000) + 1) * 96));
+        int offset1 = (int)((chunk.world.noiseGen.Generate(pos.x* 1000, pos.y* 1000, pos.z * 1000) + 1) * 16);
         block.data3 = (byte)((block.data3 & 240) | (offset1 & 15));
-        int offset2 = (int)((chunk.world.noiseGen.Generate(pos.x*100, pos.y * 100 + 1000, pos.z * 100) + 1) * 16);
+        int offset2 = (int)((chunk.world.noiseGen.Generate(pos.x*1000, pos.y * 10000, pos.z * 1000) + 1) * 16);
         block.data3 = (byte)((offset2 << 4) | (block.data3 & 15));
 
         return block;
