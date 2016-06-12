8e6c565991c90bda12285f167f15ff1283524bd7
bielyr@outlook.com <bielyr@outlook.com>
2016-06-12 14:16:33 +0200

    [F] Debug version of config.cs was accidentally commited

--- Assets/Voxelmetric/Code/Utilities/Config.cs
+++ Assets/Voxelmetric/Code/Utilities/Config.cs
@@ -4,7 +4,7 @@ namespace Voxelmetric.Code.Utilities
 {
     public static class Env
     {
-        public const int ChunkPower = 4;
+        public const int ChunkPower = 5;
         public const int ChunkPower2 = 2*ChunkPower;
         public const int ChunkSize = 1 << ChunkPower;
         public const int ChunkMask = ChunkSize-1;
@@ -21,7 +21,7 @@ namespace Voxelmetric.Code.Utilities
 
     public static class Core
     {
-        public static readonly bool UseMultiThreading = false;
+        public static readonly bool UseMultiThreading = true;
     }
 
     public static class Directories
