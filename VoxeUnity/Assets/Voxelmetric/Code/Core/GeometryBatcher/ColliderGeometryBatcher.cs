fc26c65d349cd81324578a9345ddda4c5fb68372
bielyr@outlook.com <bielyr@outlook.com>
2016-07-10 21:11:13 +0200

    [F] Don't generate collider for grass

--- VoxeUnity/Assets/Voxelmetric/Code/Core/GeometryBatcher/ColliderGeometryBatcher.cs
+++ VoxeUnity/Assets/Voxelmetric/Code/Core/GeometryBatcher/ColliderGeometryBatcher.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
 using Assets.Voxelmetric.Code.Core.GeometryBatcher;
 using UnityEngine;
 using UnityEngine.Assertions;
