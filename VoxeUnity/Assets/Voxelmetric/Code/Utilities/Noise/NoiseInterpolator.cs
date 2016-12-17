852c25864cf29d4711ee4e8225c65eb686465c6e
bielyr@outlook.com <bielyr@outlook.com>
2016-12-17 12:13:56 +0100

    [A] Added ability to interpolate in one dimension [F] Fix for 3D interpolation

--- VoxeUnity/Assets/Voxelmetric/Code/Utilities/Noise/NoiseInterpolator.cs
+++ VoxeUnity/Assets/Voxelmetric/Code/Utilities/Noise/NoiseInterpolator.cs
@@ -7,7 +7,7 @@ namespace Voxelmetric.Code.Utilities.Noise
         //! Downsampled size of chunk
         protected int m_size;
         protected int m_sizePow2;
-        protected int m_sizePow3;
+        protected int m_sizePow2plusSize;
         //! +1 intepolated into downsampled state
         protected int m_step;
         //! Interpolated scale
@@ -16,13 +16,14 @@ namespace Voxelmetric.Code.Utilities.Noise
         /// <summary>
         /// Initiates NoiseInterpolator
         /// </summary>
+        /// <param name="size">Value which we want to downsample. When calling Interpolate, the coordinates must be within 0..size-1 range</param>
         /// <param name="downsamplingFactor">Factor says how much is ChunkSize supposed to be downsampled</param>
-        public void SetInterpBitStep(int downsamplingFactor)
+        public void SetInterpBitStep(int size, int downsamplingFactor)
         {
             m_step = downsamplingFactor;
-            m_size = (Env.ChunkSize>>m_step)+1;
+            m_size = (size >> m_step)+1;
             m_sizePow2 = m_size*m_size;
-            m_sizePow3 = m_sizePow2*m_size;
+            m_sizePow2plusSize = m_sizePow2+m_size;
             m_scale = 1f/(1<<m_step);
         }
 
@@ -30,10 +31,26 @@ namespace Voxelmetric.Code.Utilities.Noise
         public int Size { get { return m_size; } }
 
         /// <summary>
-        /// Interpolate chunk coordinates into downsampled coordinates of a lookuptable and returns and interpolated value from it
+        /// Interpolates the given coordinate into a downsampled coordinate and returns a value from the lookup table on that position
         /// </summary>
-        /// <param name="x">Position within chunk on the x axis</param>
-        /// <param name="z">Position within chunk on the z axis</param>
+        /// <param name="x">Position on the x axis</param>
+        /// <param name="lookupTable">Lookup table to be used to interpolate</param>
+        public float Interpolate(int x, float[] lookupTable)
+        {
+            float xs = (x + 0.5f) * m_scale;
+
+            int x0 = Helpers.FastFloor(xs);
+
+            xs = (xs - x0);
+
+            return Helpers.Interpolate(lookupTable[x0], lookupTable[x0 + 1], xs);
+        }
+
+        /// <summary>
+        /// Interpolates given coordinates into downsampled coordinates and returns a value from the lookup table on that position
+        /// </summary>
+        /// <param name="x">Position on the x axis</param>
+        /// <param name="z">Position on the z axis</param>
         /// <param name="lookupTable">Lookup table to be used to interpolate</param>
         public float Interpolate(int x, int z, float[] lookupTable)
         {
@@ -56,11 +73,11 @@ namespace Voxelmetric.Code.Utilities.Noise
         }
 
         /// <summary>
-        /// Interpolate chunk coordinates into downsampled coordinates of a lookuptable and returns and interpolated value from it
+        /// Interpolates given coordinates into downsampled coordinates and returns a value from the lookup table on that position
         /// </summary>
-        /// <param name="x">Position within chunk on the x axis</param>
-        /// <param name="y">Position within chunk on the y axis</param>
-        /// <param name="z">Position within chunk on the z axis</param>
+        /// <param name="x">Position on the x axis</param>
+        /// <param name="y">Position on the y axis</param>
+        /// <param name="z">Position on the z axis</param>
         /// <param name="lookupTable">Lookup table to be used to interpolate</param>
         public float Interpolate(int x, int y, int z, float[] lookupTable)
         {
@@ -79,7 +96,7 @@ namespace Voxelmetric.Code.Utilities.Noise
             int lookupIndex = Helpers.GetIndex1DFrom3D(x0, y0, z0, m_size, m_size);
             int lookupIndexY = lookupIndex+m_sizePow2; // x0, y0+1, z0
             int lookupIndexZ = lookupIndex+m_size;  // x0, y0, z0+1
-            int lookupIndexYZ = lookupIndex+m_sizePow3; // x0, y0+1, z0+1
+            int lookupIndexYZ = lookupIndex+ m_sizePow2plusSize; // x0, y0+1, z0+1
 
             return Helpers.Interpolate(
                 Helpers.Interpolate(
