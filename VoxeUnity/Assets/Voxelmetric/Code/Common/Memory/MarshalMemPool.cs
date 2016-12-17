64fbb6839c19a3dc315d727186b93558914c077c
bielyr@outlook.com <bielyr@outlook.com>
2016-12-17 11:21:08 +0100

    [F] Non-debug builds would not build

--- VoxeUnity/Assets/Voxelmetric/Code/Common/Memory/MarshalMemPool.cs
+++ VoxeUnity/Assets/Voxelmetric/Code/Common/Memory/MarshalMemPool.cs
@@ -6,10 +6,8 @@ namespace Voxelmetric.Code.Common.Memory
 {
     public class MarshalMemPool
     {
-#if DEBUG
         //! Allocated memory in bytes
         private readonly int m_size;
-#endif
         //! Position to the beggining of the buffer
         private readonly long m_buffer;
         //! Current position in allocate array (m_buffer+x)
@@ -17,9 +15,7 @@ namespace Voxelmetric.Code.Common.Memory
 
         public MarshalMemPool(int initialSize)
         {
-#if DEBUG
             m_size = initialSize;
-#endif
             // Allocate all memory we can
             m_buffer = (long)Marshal.AllocHGlobal(initialSize);
             m_pos = m_buffer;
@@ -33,10 +29,8 @@ namespace Voxelmetric.Code.Common.Memory
 
         public IntPtr Pop(int size)
         {
-#if DEBUG
             // Do not take more than we can give!
             Assert.IsTrue(m_pos+size<m_buffer+m_size);
-#endif
 
             m_pos += size;
             return (IntPtr)m_pos;
@@ -44,10 +38,8 @@ namespace Voxelmetric.Code.Common.Memory
 
         public void Push(int size)
         {
-#if DEBUG
             // Do not return than we gave!
             Assert.IsTrue(m_pos>=m_buffer);
-#endif
 
             m_pos -= size;
         }
