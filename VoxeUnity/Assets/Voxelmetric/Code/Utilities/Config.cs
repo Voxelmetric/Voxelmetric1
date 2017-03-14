5a0aff341d675c79d91eebd1013d734f4ec18645
bielyr@outlook.com <bielyr@outlook.com>
2017-03-14 19:26:07 +0100

    [U] Runtime static readonlies in Config.cs changed to compile-time consts

--- VoxeUnity/Assets/Voxelmetric/Code/Utilities/Config.cs
+++ VoxeUnity/Assets/Voxelmetric/Code/Utilities/Config.cs
@@ -4,25 +4,25 @@ namespace Voxelmetric.Code.Utilities
 {
     public static class Env
     {
-        public static readonly int ChunkPow = 5;
-        public static readonly int ChunkPow2 = 2*ChunkPow;
-        public static readonly int ChunkSize = 1 << ChunkPow;
-        public static readonly int ChunkSizeHalf = ChunkSize>>1;
-        public static readonly int ChunkSizePow2 = ChunkSize*ChunkSize;
-        public static readonly int ChunkSizePow3 = ChunkSize*ChunkSizePow2;
-        public static readonly int ChunkMask = ChunkSize-1;
+        public const int ChunkPow = 5;
+        public const int ChunkPow2 = 2*ChunkPow;
+        public const int ChunkSize = 1 << ChunkPow;
+        public const int ChunkSizeHalf = ChunkSize>>1;
+        public const int ChunkSizePow2 = ChunkSize*ChunkSize;
+        public const int ChunkSizePow3 = ChunkSize*ChunkSizePow2;
+        public const int ChunkMask = ChunkSize-1;
 
 
         //! Internal chunk size including room for edge fields as well so that we do not have to check whether we are within chunk bounds.
         //! This means we will ultimately consume a bit more memory in exchange for more performance
-        public static readonly int ChunkPadding = 1;
-        public static readonly int ChunkSizePlusPadding = ChunkSize + ChunkPadding;
-        public static readonly int ChunkSizeWithPadding = ChunkSize + ChunkPadding * 2;
-        public static readonly int ChunkSizeWithPaddingPow2 = ChunkSizeWithPadding*ChunkSizeWithPadding;
-        public static readonly int ChunkSizeWithPaddingPow3 = ChunkSizeWithPadding*ChunkSizeWithPaddingPow2;
+        public const int ChunkPadding = 1;
+        public const int ChunkSizePlusPadding = ChunkSize + ChunkPadding;
+        public const int ChunkSizeWithPadding = ChunkSize + ChunkPadding * 2;
+        public const int ChunkSizeWithPaddingPow2 = ChunkSizeWithPadding*ChunkSizeWithPadding;
+        public const int ChunkSizeWithPaddingPow3 = ChunkSizeWithPadding*ChunkSizeWithPaddingPow2;
 
-        public static readonly float BlockSize = 1f;
-        public static readonly float BlockSizeInv = 1f / BlockSize;
+        public const float BlockSize = 1f;
+        public const float BlockSizeInv = 1f / BlockSize;
         public static readonly Vector3Int ChunkSizePos = Vector3Int.one * ChunkSize;
 
         // Padding added to the size of block faces to fix floating point issues
@@ -32,12 +32,12 @@ namespace Voxelmetric.Code.Utilities
 
     public static class Core
     {
-        public static readonly bool UseThreadPool = true;
-        public static readonly bool UseThreadedIO = true;
+        public const bool UseThreadPool = true;
+        public const bool UseThreadedIO = true;
     }
 
     public static class Directories
     {
-        public static readonly string SaveFolder = "VoxelSaves";
+        public const string SaveFolder = "VoxelSaves";
     }
 }
