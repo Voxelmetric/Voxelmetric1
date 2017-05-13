27bd6d42122b611ef5f05d0f01cbcfe66e467a57
bielyr@outlook.com <bielyr@outlook.com>
2017-05-13 14:55:58 +0200

    [O] Minor tweak to AO to eliminate some bool->int conversions

--- VoxeUnity/Assets/Voxelmetric/Code/Data types/BlockLightData.cs
+++ VoxeUnity/Assets/Voxelmetric/Code/Data types/BlockLightData.cs
@@ -26,14 +26,23 @@ namespace Voxelmetric.Code.Data_types
             bool seSolid, bool sSolid, bool swSolid, bool wSolid
         )
         {
+            int _nwSolid = nwSolid ? 1 : 0;
+            int _nSolid = nSolid ? 1 : 0;
+            int _neSolid = neSolid ? 1 : 0;
+            int _eSolid = eSolid ? 1 : 0;
+            int _seSolid = seSolid ? 1 : 0;
+            int _sSolid = sSolid ? 1 : 0;
+            int _swSolid = swSolid ? 1 : 0;
+            int _wSolid = wSolid ? 1 : 0;
+
             // sw
-            int sw = GetVertexAO(sSolid, wSolid, swSolid);
+            int sw = GetVertexAO(_sSolid, _wSolid, _swSolid);
             // nw
-            int nw = GetVertexAO(nSolid, wSolid, nwSolid);
+            int nw = GetVertexAO(_nSolid, _wSolid, _nwSolid);
             // ne
-            int ne = GetVertexAO(nSolid, eSolid, neSolid);
+            int ne = GetVertexAO(_nSolid, _eSolid, _neSolid);
             // se
-            int se = GetVertexAO(sSolid, eSolid, seSolid);
+            int se = GetVertexAO(_sSolid, _eSolid, _seSolid);
 
             mask = (short)(sw|(nw<<2)|(ne<<4)|(se<<6));
 
@@ -42,18 +51,11 @@ namespace Voxelmetric.Code.Data_types
                 mask |= (1<<8);
         }
 
-        private static int GetVertexAO(bool side1, bool side2, bool corner)
+        private static int GetVertexAO(int side1, int side2, int corner)
         {
-            if (side1 && side2)
-                return 3;
-
-            int s1 = side1 ? 1 : 0;
-            int s2 = side2 ? 1 : 0;
-            int c = corner ? 1 : 0;
-
-            return s1 + s2 + c;
+            return (side1+side2==2) ? 3 : side1+side2+corner;
         }
-        
+
         public int swAO
         {
             get { return (mask&0x03); }
