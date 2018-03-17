Voxelmetric/Voxelmetric1

commit 727b270b01c4aac51515b1757cd293e55853fb49

[U] WorldChunks.cs cleanup [O] Don't check if chunk fits within the configured world boundaries when calling Get

VoxeUnity/Assets/Voxelmetric/Code/Core/WorldChunks.cs
@@ -5,22 +5,20 @@ using Voxelmetric.Code.Data_types;
 
 namespace Voxelmetric.Code.Core
 {
+    /// <summary>
+    /// Chunk storage. All chunks in the world are stored here
+    /// </summary>
     public class WorldChunks
     {
-        World world;
-        Dictionary<Vector3Int, Chunk> chunks = new Dictionary<Vector3Int, Chunk>();
-
-        /*[ThreadStatic]
-        private static Vector3Int lastChunkPos;
-        [ThreadStatic]
-        private static Chunk lastChunk;*/
-
-        public ICollection<Chunk> chunkCollection
+        private World world;
+        private Dictionary<Vector3Int, Chunk> chunks = new Dictionary<Vector3Int, Chunk>();
+        
+        public ICollection<Chunk> Chunks
         {
             get { return chunks.Values; }
         }
 
-        public ICollection<Vector3Int> posCollection
+        public ICollection<Vector3Int> Positions
         {
             get { return chunks.Keys; }
         }
@@ -35,91 +33,52 @@ namespace Voxelmetric.Code.Core
             this.world = world;
         }
 
-        /// <summary> Returns the chunk at the given position </summary>
-        /// <param name="pos">Position of the chunk in the world coordinates</param>
-        /// <returns>The chunk that contains the given block position or null if there is none</returns>
-        public Chunk Get(ref Vector3Int pos)
-        {
-            Vector3Int p = Chunk.ContainingChunkPos(ref pos);
-
-            // If we previously searched for this chunk there is no need to look it up again
-            /*if (p == lastChunkPos && lastChunk != null)
-                return lastChunk;
-
-            lastChunkPos = p;*/
-
-            Chunk containerChunk;
-            chunks.TryGetValue(p, out containerChunk);
-
-            return containerChunk;
-        }
-
-        public bool Set(ref Vector3Int pos, Chunk chunk)
+        /// <summary>Instantiates a new chunk at a given position</summary>
+        /// <param name="pos">Position to create this chunk on in the world coordinates</param>
+        /// <param name="chunk">Chunk at a given positon</param>
+        /// <returns>Trus if a new chunk was created. False otherwise</returns>
+        public bool Create(ref Vector3Int pos, out Chunk chunk)
         {
             Assert.IsTrue(Helpers.IsMainThread);
             Vector3Int p = Chunk.ContainingChunkPos(ref pos);
+
+            chunk = null;
 
             // Let's keep it within allowed world bounds
             if (!world.IsCoordInsideWorld(ref p))
                 return false;
+            
+            chunk = Get(ref p);
+            if (chunk == null)
+            {
+                // Create a new chunk if it does not exist yet
+                chunk = Chunk.CreateChunk(world, p);
+                chunks.Add(p, chunk);
+                return true;
+            }
 
-            chunks[p] = chunk;
-            return true;
+            return false;
         }
 
-        /// <summary>Removes a given chunk from the world</summary>
+        /// <summary>Removes a chunk from the world</summary>
         /// <param name="chunk">Chunk to be removed from the world</param>
-        public void RemoveChunk(Chunk chunk)
+        public void Remove(Chunk chunk)
         {
             Assert.IsTrue(Helpers.IsMainThread);
             Assert.IsNotNull(chunk);
-            /*if (chunk == lastChunk)
-                lastChunk = null;*/
 
             Chunk.RemoveChunk(chunk);
             chunks.Remove(chunk.pos);
         }
 
-        /// <summary>Instantiates a new chunk at a given position. If the chunk already exists, it returns it</summary>
-        /// <param name="pos">Position to create this chunk on in the world coordinates.</param>
-        /// <param name="chunk">Chunk at a given world position</param>
-        /// <returns>True if a new chunk was created. False otherwise</returns>
-        public bool CreateOrGetChunk(ref Vector3Int pos, out Chunk chunk)
+        /// <summary>Returns a chunk at a given position</summary>
+        /// <param name="pos">Position of the chunk in the world coordinates</param>
+        /// <returns>A chunk at a given position</returns>
+        public Chunk Get(ref Vector3Int pos)
         {
-            Assert.IsTrue(Helpers.IsMainThread);
-            Vector3Int p = Chunk.ContainingChunkPos(ref pos);
-
-            // Let's keep it within allowed world bounds
-            if (!world.IsCoordInsideWorld(ref p))
-            {
-                chunk = null;
-                return false;
-            }
-
-            /*// If we previously searched for this chunk there is no need to look it up again
-            if (pos==lastChunkPos && lastChunk!=null)
-            {
-                chunk = lastChunk;
-                return false;
-            }
-
-            lastChunkPos = pos;*/
-
-            // Don't recreate the chunk if it already exists
-            chunk = Get(ref p);
-            if (chunk!=null)
-                return false;
-
-            // Create a new chunk
-            chunk = Chunk.CreateChunk(world, p);
-
-            lock (chunks)
-            {
-                chunks.Add(p, chunk);
-            }
-
-            return true;
+            Chunk containerChunk;
+            chunks.TryGetValue(pos, out containerChunk);
+            return containerChunk;
         }
-
     }
 }

VoxeUnity/Assets/Voxelmetric/Code/Utilities/ChunkLoaders/LoadChunks.cs
@@ -352,7 +352,7 @@ namespace Voxelmetric.Code.Utilities.ChunkLoaders
                         // Create a new chunk if possible
                         Vector3Int newChunkPos = new Vector3Int(cx, cy, cz);
                         Chunk chunk;
-                        if (!chunks.CreateOrGetChunk(ref newChunkPos, out chunk))
+                        if (!chunks.Create(ref newChunkPos, out chunk))
                             continue;
 
                         if (FullLoadOnStartUp)
@@ -404,7 +404,7 @@ namespace Voxelmetric.Code.Utilities.ChunkLoaders
                 if (stateManager.IsStateCompleted(ChunkState.Remove))
                 {
                     // Remove the chunk from our provider and unregister it from chunk storage
-                    world.chunks.RemoveChunk(chunk);
+                    world.chunks.Remove(chunk);
 
                     // Unregister from updates
                     m_updateRequests.RemoveAt(i);
@@ -547,7 +547,7 @@ namespace Voxelmetric.Code.Utilities.ChunkLoaders
 
             if (world!=null && world.chunks!=null && (Diag_DrawWorldBounds || Diag_DrawLoadRange))
             {
-                foreach (Chunk chunk in world.chunks.chunkCollection)
+                foreach (Chunk chunk in world.chunks.Chunks)
                 {
                     if (Diag_DrawWorldBounds)
                     {

VoxeUnity/Assets/Voxelmetric/Code/Utilities/ChunkLoaders/LoadChunksSimple.cs
@@ -354,7 +354,7 @@ namespace Voxelmetric.Code.Utilities.ChunkLoaders
                         // Create a new chunk if possible
                         Vector3Int newChunkPos = new Vector3Int(cx, cy, cz);
                         Chunk chunk;
-                        if (!chunks.CreateOrGetChunk(ref newChunkPos, out chunk))
+                        if (!chunks.Create(ref newChunkPos, out chunk))
                             continue;
 
                         if (FullLoadOnStartUp)
@@ -397,7 +397,7 @@ namespace Voxelmetric.Code.Utilities.ChunkLoaders
                 if (stateManager.IsStateCompleted(ChunkState.Remove))
                 {
                     // Remove the chunk from our provider and unregister it from chunk storage
-                    world.chunks.RemoveChunk(chunk);
+                    world.chunks.Remove(chunk);
 
                     // Unregister from updates
                     m_updateRequests.RemoveAt(i);
@@ -537,7 +537,7 @@ namespace Voxelmetric.Code.Utilities.ChunkLoaders
 
             if (world!=null && world.chunks!=null && (Diag_DrawWorldBounds || Diag_DrawLoadRange))
             {
-                foreach (Chunk chunk in world.chunks.chunkCollection)
+                foreach (Chunk chunk in world.chunks.Chunks)
                 {
                     if (Diag_DrawWorldBounds)
                     {

VoxeUnity/Assets/Voxelmetric/Code/Voxelmetric.cs
@@ -39,7 +39,7 @@ namespace Voxelmetric.Code
 
             List<Chunk> chunksToSave = new List<Chunk> ();
 
-            foreach (Chunk chunk in world.chunks.chunkCollection)
+            foreach (Chunk chunk in world.chunks.Chunks)
             {
                 // Ignore chunks that can't be saved at the moment
                 ChunkStateManagerClient stateManager = chunk.stateManager;

commit 9be972658ef2d9f212f53793e6a4a589ef6c3509

[F] Regression in 727b270b01c4aac51515b1757cd293e55853fb49

VoxeUnity/Assets/Voxelmetric/Code/Core/WorldBlocks.cs
@@ -21,8 +21,11 @@ namespace Voxelmetric.Code.Core
         /// <param name="pos">Global position of the block data</param>
         public BlockData Get(ref Vector3Int pos)
         {
+            // Transform the position into chunk coordinates
+            Vector3Int chunkPos = Chunk.ContainingChunkPos(ref pos);
+
             // Return air for chunk that do not exist
-            Chunk chunk = world.chunks.Get(ref pos);
+            Chunk chunk = world.chunks.Get(ref chunkPos);
             if (chunk==null)
                 return BlockProvider.AirBlock;
 
@@ -35,9 +38,12 @@ namespace Voxelmetric.Code.Core
 
         public BlockData Get(Vector3Int pos)
         {
-            // Return air for chunk that do not exist
-            Chunk chunk = world.chunks.Get(ref pos);
+            // Transform the position into chunk coordinates
+            Vector3Int chunkPos = Chunk.ContainingChunkPos(ref pos);
+                        
+            Chunk chunk = world.chunks.Get(ref chunkPos);
             if (chunk==null)
+                // Return air if the chunk that do not exist
                 return BlockProvider.AirBlock;
 
             int xx = Helpers.Mod(pos.x, Env.ChunkSize);
@@ -53,9 +59,12 @@ namespace Voxelmetric.Code.Core
         /// <param name="pos">Global position of the block</param>
         public Block GetBlock(ref Vector3Int pos)
         {
-            // Return air for chunk that do not exist
-            Chunk chunk = world.chunks.Get(ref pos);
+            // Transform the position into chunk coordinates
+            Vector3Int chunkPos = Chunk.ContainingChunkPos(ref pos);
+                                
+            Chunk chunk = world.chunks.Get(ref chunkPos);
             if (chunk==null)
+                // Return air if the chunk that do not exist
                 return world.blockProvider.BlockTypes[BlockProvider.AirType];
 
             int xx = Helpers.Mod(pos.x, Env.ChunkSize);
@@ -73,7 +82,10 @@ namespace Voxelmetric.Code.Core
         /// <param name="blockData">A block to be placed on a given position</param>
         public void Set(ref Vector3Int pos, BlockData blockData)
         {
-            Chunk chunk = world.chunks.Get(ref pos);
+            // Transform the position into chunk coordinates
+            Vector3Int chunkPos = Chunk.ContainingChunkPos(ref pos);
+
+            Chunk chunk = world.chunks.Get(ref chunkPos);
             if (chunk==null)
                 return;
 
@@ -92,7 +104,10 @@ namespace Voxelmetric.Code.Core
         /// <param name="blockData">A block to be placed on a given position</param>
         public void SetRaw(ref Vector3Int pos, BlockData blockData)
         {
-            Chunk chunk = world.chunks.Get(ref pos);
+            // Transform the position into chunk coordinates
+            Vector3Int chunkPos = Chunk.ContainingChunkPos(ref pos);
+
+            Chunk chunk = world.chunks.Get(ref chunkPos);
             if (chunk==null)
                 return;
 
@@ -115,6 +130,7 @@ namespace Voxelmetric.Code.Core
             if (posFrom.x>posTo.x || posFrom.y>posTo.y || posFrom.z>posTo.z)
                 return;
 
+            // Transform positions into chunk coordinates
             Vector3Int chunkPosFrom = Chunk.ContainingChunkPos(ref posFrom);
             Vector3Int chunkPosTo = Chunk.ContainingChunkPos(ref posTo);
 
@@ -161,6 +177,7 @@ namespace Voxelmetric.Code.Core
             if (posFrom.x>posTo.x || posFrom.y>posTo.y || posFrom.z>posTo.z)
                 return;
 
+            // Transform positions into chunk coordinates
             Vector3Int chunkPosFrom = Chunk.ContainingChunkPos(ref posFrom);
             Vector3Int chunkPosTo = Chunk.ContainingChunkPos(ref posTo);
 
@@ -205,7 +222,9 @@ namespace Voxelmetric.Code.Core
         public void Modify(ref Vector3Int pos, BlockData blockData, bool setBlockModified,
             Action<ModifyBlockContext> onModified = null)
         {
+            // Transform the position into chunk coordinates
             Vector3Int chunkPos = Chunk.ContainingChunkPos(ref pos);
+
             Chunk chunk = world.chunks.Get(ref chunkPos);
             if (chunk==null)
                 return;

VoxeUnity/Assets/Voxelmetric/Code/Core/WorldChunks.cs
@@ -40,20 +40,22 @@ namespace Voxelmetric.Code.Core
         public bool Create(ref Vector3Int pos, out Chunk chunk)
         {
             Assert.IsTrue(Helpers.IsMainThread);
-            Vector3Int p = Chunk.ContainingChunkPos(ref pos);
 
             chunk = null;
 
+            // Transform the position into chunk coordinates
+            Vector3Int chunkPos = Chunk.ContainingChunkPos(ref pos);
+
             // Let's keep it within allowed world bounds
-            if (!world.IsCoordInsideWorld(ref p))
+            if (!world.IsCoordInsideWorld(ref chunkPos))
                 return false;
             
-            chunk = Get(ref p);
+            chunk = Get(ref chunkPos);
             if (chunk == null)
             {
                 // Create a new chunk if it does not exist yet
-                chunk = Chunk.CreateChunk(world, p);
-                chunks.Add(p, chunk);
+                chunk = Chunk.CreateChunk(world, chunkPos);
+                chunks.Add(chunkPos, chunk);
                 return true;
             }
 
@@ -76,6 +78,8 @@ namespace Voxelmetric.Code.Core
         /// <returns>A chunk at a given position</returns>
         public Chunk Get(ref Vector3Int pos)
         {
+            Assert.IsTrue(Chunk.ContainingChunkPos(ref pos) == pos);
+
             Chunk containerChunk;
             chunks.TryGetValue(pos, out containerChunk);
             return containerChunk;